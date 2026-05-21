e403fe0264b4bcb51fe60d3dfcec3f238be0194e
drzzln <drzzln@protonmail.com>
2026-05-21 00:01:02 -0400

    fix(Cargo.nix): SRI hash for ishou@1cfd3cf (crate2nix drift)

--- Cargo.nix
+++ Cargo.nix
@@ -4581,7 +4581,7 @@ rec {
         src = pkgs.fetchgit {
           url = "https://github.com/pleme-io/ishou";
           rev = "1cfd3cf0231c461a484df7c79309f8201ca42c0a";
-          sha256 = "0a0xf6qaaj89bll0dj4h4mgplgkww8wz6yv230zja4z8sj80y6jz";
+          sha256 = "sha256-WmF3/as+2svnZo3GLDWa2GqqfFiwjUcVLWxji1Ri6Cs=";
         };
         libName = "ishou_tokens";
         dependencies = [
