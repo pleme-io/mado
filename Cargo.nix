3317203b919d512ed4a4f25d6a92bc930564d2e8
drzzln <drzzln@protonmail.com>
2026-05-20 22:51:42 -0400

    fix(Cargo.nix): canonical SRI hash for shikumi@5db7873

--- Cargo.nix
+++ Cargo.nix
@@ -3664,7 +3664,7 @@ rec {
         src = pkgs.fetchgit {
           url = "https://github.com/pleme-io/shikumi";
           rev = "5db7873c9a109f619474228cbea15d64b4825b8a";
-          sha256 = "0wx0058js6a4z3d7ds50vj1jd3gqsfa3minlqikg2i5d12nbwb6x";
+          sha256 = "LJrkEo5yMS2OqfM7vsZje+uBfB6z4rfO6ZGzFzQTNdo=";
         };
         dependencies = [
           {
@@ -3735,7 +3735,7 @@ rec {
         src = pkgs.fetchgit {
           url = "https://github.com/pleme-io/shikumi";
           rev = "5db7873c9a109f619474228cbea15d64b4825b8a";
-          sha256 = "1nim2cs1gcwix77bgqmk3ry83svvcg3bwfzkm672scbjiq9f96ic";
+          sha256 = "LJrkEo5yMS2OqfM7vsZje+uBfB6z4rfO6ZGzFzQTNdo=";
         };
         dependencies = [
           {
